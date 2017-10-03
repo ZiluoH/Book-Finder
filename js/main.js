224027f19c9d795d6df990e82dd9c2aa6b3c9cb5
ZiluoH <ziluoh@gmail.com>
2017-10-02 22:12:20 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -25,9 +25,56 @@ function bookSearch() {
 				// create new tag to store info for each book that can put in newBook div
 				var newBookImg = document.createElement('img')
 				var newBookTitle = document.createElement('h2')
+				var newBookAuthor = document.createElement('h3')
+				var newBookDate = document.createElement('h4')
+				var newBookAnchor = document.createElement('a')
 
-				results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
-				 + "<img src='" + data.items[i].volumeInfo.imageLinks.thumbnail + "''>"
+				// add class to elements
+				newBook.className = 'newBook'
+				newBookAnchor.className = 'btn btn-primary'
+
+				// add book title to book title tag
+				newBookTitle.innerText = bookData.title
+				// test if it work
+				// console.log(newBookTitle)
+
+				// add detail to newBookAnchor
+				newBookAnchor.innerText = 'show more'
+				newBookAnchor.href = bookData.infoLink
+				// setAttribute to a tag to make it open in new tag
+				newBookAnchor.setAttribute('target','_blank')
+				
+				// add img for the book(if there exists)
+				newBookImg.src = bookData.imageLinks.thumbnail
+				newBookImg.setAttribute('alt','No Image')
+
+				// add publish date
+				if (bookData.publishedDate) {
+					newBookDate.innerText = bookData.publishedDate
+				} else {
+					newBookDate.innerText = 'No publish date found'
+				}
+
+				// add author info
+				if (bookData.authors) {
+					newBookAuthor.innerText = bookData.authors
+				} else {
+					newBookAuthor.innerText = 'No author found'
+				}
+
+				// add all tag to div
+				newBook.appendChild(newBookImg)
+				newBook.appendChild(newBookTitle)
+				newBook.appendChild(newBookAuthor)
+				newBook.appendChild(newBookDate)
+				newBook.appendChild(newBookAnchor)
+
+				// add the whole div the document
+				var results = document.getElementById('results')
+				results.appendChild(newBook)
+
+				// results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>"
+				//  + "<img src='" + data.items[i].volumeInfo.imageLinks.thumbnail + "''>"
 
 				// console.log(data)
 			}
